3f2b5110d9b439c9234c70e674adc622da1da0e2
Rishi Kothari <itsrishikothari@gmail.com>
2020-06-21 20:15:13 -0400

    inputs

---
+++ courses/basics/variables/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int number;
+
+    cout << "enter a number: ";
+
+    cin >> number;
+
+    cout << "the number is: " << number << endl;
+}
