361f058359104ac5e9004cddb5d2693bf9e34e9c
Rishi Kothari <itsrishikothari@gmail.com>
2020-06-21 20:15:07 -0400

    basic variables

---
+++ courses/basics/variables/addition.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// using namespace std;
+
+// int add (x, y) {
+//     return x + y;
+// }
+
+// int main () {
+//     int x , y;
+//     cin >> x >> y;
+
+//     cout << add (x, y);
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long int x = 5000000000LL; // 5 billion 
+    long long int y = 5000000000LL; // 5 billion  
+    cout << "product: " << x * y << endl; 
+    return 0;
+}
